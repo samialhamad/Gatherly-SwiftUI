c4af8e9c80970b1d9f06cffb20e2b5dbd3bb7512
samialhamad <40968198+samialhamad@users.noreply.github.com>
2025-03-04 14:37:58 -0800

    Added EventRow View

---
+++ Gatherly-SwiftUI/Views/Components/EventRow.swift
@@ -0,0 +1,42 @@
+//
+//  EventRow.swift
+//  Gatherly-SwiftUI
+//
+//  Created by Sami Alhamad on 3/4/25.
+//
+
+import SwiftUI
+
+struct EventRow: View {
+    let event: Event
+    
+    var body: some View {
+        HStack {
+            Image(systemName: "calendar.circle")
+                .font(.largeTitle)
+                .foregroundColor(.blue)
+            
+            VStack(alignment: .leading) {
+                Text(event.title ?? "Untitled Event")
+                    .font(.headline)
+                Text(event.description ?? "No description")
+                    .font(.subheadline)
+                    .foregroundColor(.gray)
+            }
+            
+            Spacer()
+            
+            Text(event.startTimestamp != nil ? formattedTime(event.startTimestamp!) : "")
+                .font(.subheadline)
+                .foregroundColor(.gray)
+        }
+        .padding(.vertical, 5)
+    }
+    
+    func formattedTime(_ timestamp: Int) -> String {
+        let date = Date(timeIntervalSince1970: TimeInterval(timestamp))
+        let formatter = DateFormatter()
+        formatter.timeStyle = .short
+        return formatter.string(from: date)
+    }
+}
