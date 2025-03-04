40b9a680b20494e8234f491eb3a5c2ce0baa02e4
samialhamad <40968198+samialhamad@users.noreply.github.com>
2025-03-04 13:40:46 -0800

    Added Message Model

---
+++ Gatherly-SwiftUI/Model/Message/Message.swift
@@ -0,0 +1,17 @@
+//
+//  Message.swift
+//  Gatherly-SwiftUI
+//
+//  Created by Sami Alhamad on 3/4/25.
+//
+
+import Foundation
+
+struct Message: Equatable {
+    var id: Int?
+    var eventID: Int?
+    var timestamp: Int?
+    var userID: Int
+    var message: String
+    var read: Bool?
+}
---
+++ Gatherly-SwiftUI/Model/Message/MessageTests.swift
@@ -0,0 +1,26 @@
+//
+//  MessageTests.swift
+//  Gatherly-SwiftUITests
+//
+//  Created by Sami Alhamad on 3/4/25.
+//
+
+import XCTest
+@testable import Gatherly_SwiftUI
+
+final class MessageTests: XCTestCase {
+
+    //MARK: - Equatable
+    
+    func testEqualTrue() {
+        let message0 = Message(id: 0, userID: 0, message: "", read: true)
+        let message1 = Message(id: 0, userID: 0, message: "", read: true)
+        XCTAssertEqual(message0, message1)
+    }
+    
+    func testEqualFalse() {
+        let message0 = Message(id: 0, userID: 0, message: "", read: true)
+        let message1 = Message(id: 0, userID: 0, message: "", read: false)
+        XCTAssertNotEqual(message0, message1)
+    }
+}
