a6edb75ee90e8466c61e48947bd82d9ef7b4ea37
samialhamad <40968198+samialhamad@users.noreply.github.com>
2025-03-04 12:09:48 -0800

    Initial Commit

---
+++ Gatherly-SwiftUI/Gatherly_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Gatherly_SwiftUIApp.swift
+//  Gatherly-SwiftUI
+//
+//  Created by Sami Alhamad on 3/4/25.
+//
+
+import SwiftUI
+
+@main
+struct Gatherly_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Gatherly-SwiftUIUITests/Gatherly_SwiftUIUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  Gatherly_SwiftUIUITestsLaunchTests.swift
+//  Gatherly-SwiftUIUITests
+//
+//  Created by Sami Alhamad on 3/4/25.
+//
+
+import XCTest
+
+final class Gatherly_SwiftUIUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
