38f424194598ac538c34d8f63a4fcfb18468b89a
samialhamad <40968198+samialhamad@users.noreply.github.com>
2025-03-04 14:37:52 -0800

    Updated ContentView for first UI prototype

---
+++ Gatherly-SwiftUI/Views/ContentView.swift
@@ -0,0 +1,39 @@
+//
+//  ContentView.swift
+//  Gatherly-SwiftUI
+//
+//  Created by Sami Alhamad on 3/4/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        TabView {
+            CalendarView(events: SampleData.sampleEvents) // Pass user events
+                .tabItem {
+                    Label("My Events", systemImage: "calendar")
+                }
+            
+            Text("Create Event")
+                .tabItem {
+                    Label("Create", systemImage: "plus")
+                }
+            
+            Text("Friends")
+                .tabItem {
+                    Label("Friends", systemImage: "heart")
+                }
+            
+            Text("Profile")
+                .tabItem {
+                    Label("Profile", systemImage: "person")
+                }
+        }
+    }
+}
+
+
+#Preview {
+    ContentView()
+}
