fbfcc315068516499944c42b986ab89a8f16493d
samialhamad <40968198+samialhamad@users.noreply.github.com>
2025-03-05 18:46:27 -0800

    Added DateUtils helper

---
+++ Gatherly-SwiftUI/Helpers/Date/DateUtils.swift
@@ -0,0 +1,27 @@
+//
+//  DateUtils.swift
+//  Gatherly-SwiftUI
+//
+//  Created by Sami Alhamad on 3/5/25.
+//
+
+import Foundation
+
+struct DateUtils {
+    static func merge(date: Date, time: Date) -> Date {
+        let calendar = Calendar.current
+        let dateComponents = calendar.dateComponents([.year, .month, .day], from: date)
+        let timeComponents = calendar.dateComponents([.hour, .minute, .second], from: time)
+        
+        var mergedComponents = DateComponents()
+        mergedComponents.year = dateComponents.year
+        mergedComponents.month = dateComponents.month
+        mergedComponents.day = dateComponents.day
+        mergedComponents.hour = timeComponents.hour
+        mergedComponents.minute = timeComponents.minute
+        mergedComponents.second = timeComponents.second
+        
+        return calendar.date(from: mergedComponents) ?? time
+    }
+}
+
