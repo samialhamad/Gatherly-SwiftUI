78d45b08de17ae94145aeaec30a90ac7bfae44ec
samialhamad <40968198+samialhamad@users.noreply.github.com>
2025-03-07 12:09:46 -0800

    Changed DateUtils to Date extension

---
+++ Gatherly-SwiftUI/Extensions/Date/Date.swift
@@ -0,0 +1,196 @@
+//
+//  Date.swift
+//  Gatherly-SwiftUI
+//
+//  Created by Sami Alhamad on 3/5/25.
+//
+
+import Foundation
+
+extension Date {
+    
+    //MARK: - Timestamp
+    
+    public init(_ timestamp: Int) {
+        self.init(timeIntervalSince1970: TimeInterval(timestamp))
+    }
+    
+    public var timestamp: Int {
+        return Int(timeIntervalSince1970)
+    }
+    
+    public var dayTimestamp: Int? {
+        let calendar = Calendar.current
+        
+        var components = calendar.dateComponents([.year, .month, .day], from: self)
+        components.hour = 0
+        components.minute = 0
+        
+        let day = calendar.date(from: components)
+        return day?.timestamp
+    }
+    
+    //MARK: - Plus / Minus
+    
+    public func plus(
+        calendarComponent: Calendar.Component,
+        value: Int
+    ) -> Date? {
+        return Calendar.current.date(
+            byAdding: calendarComponent,
+            value: value,
+            to: self
+        )
+    }
+    
+    public func minus(
+        calendarComponent: Calendar.Component,
+        value: Int
+    ) -> Date? {
+        let value = -value
+        return Calendar.current.date(
+            byAdding: calendarComponent,
+            value: value,
+            to: self
+        )
+    }
+    
+    //MARK: - Between
+    
+    public static func yearsBetween(_ date1: Date, _ date2: Date) -> Int {
+        let calendar = Calendar.current
+        let components = calendar.dateComponents([.year],
+                                                 from: date1,
+                                                 to: date2)
+        return components.year!
+    }
+    
+    public static func monthsBetween(_ date1: Date, _ date2: Date) -> Int {
+        let calendar = Calendar.current
+        let components = calendar.dateComponents([.month],
+                                                 from: date1,
+                                                 to: date2)
+        return components.month!
+    }
+    
+    public static func weeksBetween(_ date1: Date, _ date2: Date) -> Int {
+        let calendar = Calendar.current
+        let components = calendar.dateComponents([.weekOfYear],
+                                                 from: date1,
+                                                 to: date2)
+        return components.weekOfYear!
+    }
+    
+    public static func daysBetween(_ date1: Date, _ date2: Date) -> Int {
+        let calendar = Calendar.current
+        let components = calendar.dateComponents([.day],
+                                                 from: date1,
+                                                 to: date2)
+        return components.day!
+    }
+    
+    public static func hoursBetween(_ date1: Date, _ date2: Date) -> Int {
+        let calendar = Calendar.current
+        let components = calendar.dateComponents([.hour],
+                                                 from: date1,
+                                                 to: date2)
+        return components.hour!
+    }
+    
+    public static func minutesBetween(_ date1: Date, _ date2: Date) -> Int {
+        let calendar = Calendar.current
+        let components = calendar.dateComponents([.minute],
+                                                 from: date1,
+                                                 to: date2)
+        return components.minute!
+    }
+    
+    public static func secondsBetween(_ date1: Date, _ date2: Date) -> Int {
+        let calendar = Calendar.current
+        let components = calendar.dateComponents([.second],
+                                                 from: date1,
+                                                 to: date2)
+        return components.second!
+    }
+    
+    //MARK: - Same
+    
+    public static func isSameDay(date1: Date, date2: Date) -> Bool {
+        return Calendar.current.isDate(date1, inSameDayAs:date2)
+    }
+    
+    // MARK: - Merge
+    
+    public static func merge(date: Date, time: Date) -> Date {
+        let calendar = Calendar.current
+        let dateComponents = calendar.dateComponents([.year, .month, .day], from: date)
+        let timeComponents = calendar.dateComponents([.hour, .minute, .second], from: time)
+        
+        var mergedComponents = DateComponents()
+        mergedComponents.year = dateComponents.year
+        mergedComponents.month = dateComponents.month
+        mergedComponents.day = dateComponents.day
+        mergedComponents.hour = timeComponents.hour
+        mergedComponents.minute = timeComponents.minute
+        mergedComponents.second = timeComponents.second
+        
+        return calendar.date(from: mergedComponents) ?? time
+    }
+    
+    // MARK: - Range
+    
+    public static func startTimeRange(for selectedDate: Date) -> ClosedRange<Date> {
+        let now = Date()
+        let calendar = Calendar.current
+        if calendar.isDate(selectedDate, inSameDayAs: now) {
+            let dayEnd = calendar.date(bySettingHour: 23, minute: 59, second: 59, of: now)!
+            return now...dayEnd
+        } else {
+            let dayStart = calendar.startOfDay(for: selectedDate)
+            let dayEnd = calendar.date(bySettingHour: 23, minute: 59, second: 59, of: selectedDate)!
+            return dayStart...dayEnd
+        }
+    }
+    
+    public static func endTimeRange(for selectedDate: Date, startTime: Date) -> ClosedRange<Date> {
+        let now = Date()
+        let calendar = Calendar.current
+        if calendar.isDate(selectedDate, inSameDayAs: now) {
+            let dayEnd = calendar.date(bySettingHour: 23, minute: 59, second: 59, of: now)!
+            let lowerBound = max(startTime, now)
+            return lowerBound...dayEnd
+        } else {
+            let dayStart = calendar.startOfDay(for: selectedDate)
+            let dayEnd = calendar.date(bySettingHour: 23, minute: 59, second: 59, of: selectedDate)!
+            return dayStart...dayEnd
+        }
+    }
+    
+    //MARK: - Time String
+    
+    public static func timeString(date1: Date, date2: Date) -> String {
+        let secondsBetween = Date.secondsBetween(date1, date2)
+        
+        let days = secondsBetween / 86400
+        let hours = secondsBetween / 3600
+        let minutes = secondsBetween / 60 % 60
+        let seconds = secondsBetween % 60
+        
+        switch hours {
+        case 24...:
+            let format = "%id"
+            return String(format:format, days)
+        case 10..<24:
+            let format = "%02i:%02i:%02i"
+            return String(format:format, hours, minutes, seconds)
+        case 1..<10:
+            let format = "%01i:%02i:%02i"
+            return String(format:format, hours, minutes, seconds)
+        case ..<1:
+            let format = "%02i:%02i"
+            return String(format:format, minutes, seconds)
+        default:
+            return ""
+        }
+    }
+}
---
+++ Gatherly-SwiftUI/Extensions/Date/DateTests.swift
@@ -0,0 +1,240 @@
+//
+//  DateUtilsTests.swift
+//  Gatherly-SwiftUITests
+//
+//  Created by Sami Alhamad on 3/5/25.
+//
+
+import XCTest
+@testable import Gatherly_SwiftUI
+
+final class DateTests: XCTestCase {
+    
+    //MARK: - Timestamp
+
+    func testTimestamp() {
+        let timestamp = 780192000
+        let date = Date(timestamp)
+        XCTAssertEqual(date.timestamp, timestamp)
+    }
+    
+    func testDayTimestamp() {
+        let timestamp = 780192000
+        let date = Date(timestamp)
+        let dayTimestamp = date.dayTimestamp
+        XCTAssertEqual(780130800, dayTimestamp)
+    }
+    
+    //MARK: - Plus / Minus + Between
+
+    //years
+    
+    func testDatePlusYearsAndYearsBetween() {
+        let date1 = Date()
+        let date2 = Date().plus(calendarComponent: .year, value: 3)!
+        let yearsBetween = Date.yearsBetween(date1, date2)
+        XCTAssertEqual(yearsBetween, 3)
+    }
+    
+    func testDateMinusYearsAndYearsBetween() {
+        let date1 = Date().minus(calendarComponent: .year, value: 3)!
+        let date2 = Date()
+        let yearsBetween = Date.yearsBetween(date1, date2)
+        XCTAssertEqual(yearsBetween, 3)
+    }
+    
+    //months
+    
+    func testDatePlusMonthsAndMonthsBetween() {
+        let date1 = Date()
+        let date2 = Date().plus(calendarComponent: .month, value: 6)!
+        let monthsBetween = Date.monthsBetween(date1, date2)
+        XCTAssertEqual(monthsBetween, 6)
+    }
+    
+    func testDateMinusMonthsAndMonthsBetween() {
+        let date1 = Date().minus(calendarComponent: .month, value: 6)!
+        let date2 = Date()
+        let monthsBetween = Date.monthsBetween(date1, date2)
+        XCTAssertEqual(monthsBetween, 6)
+    }
+    
+    //weeks
+    
+    func testDatePlusWeeksAndWeeksBetween() {
+        let date1 = Date()
+        let date2 = Date().plus(calendarComponent: .weekOfYear, value: 1)!
+        let weeksBetween = Date.weeksBetween(date1, date2)
+        XCTAssertEqual(weeksBetween, 1)
+    }
+    
+    func testDateMinusWeeksAndWeeksBetween() {
+        let date1 = Date().minus(calendarComponent: .weekOfYear, value: 1)!
+        let date2 = Date()
+        let weeksBetween = Date.weeksBetween(date1, date2)
+        XCTAssertEqual(weeksBetween, 1)
+    }
+    
+    //days
+
+    func testDatePlusDaysAndDaysBetween() {
+        let date1 = Date()
+        let date2 = Date().plus(calendarComponent: .day, value: 4)!
+        let daysBetween = Date.daysBetween(date1, date2)
+        XCTAssertEqual(daysBetween, 4)
+    }
+    
+    func testDateMinusDaysAndDaysBetween() {
+        let date1 = Date().minus(calendarComponent: .day, value: 4)!
+        let date2 = Date()
+        let daysBetween = Date.daysBetween(date1, date2)
+        XCTAssertEqual(daysBetween, 4)
+    }
+    
+    //hours
+    
+    func testDatePlusHoursAndHoursBetween() {
+        let date1 = Date()
+        let date2 = Date().plus(calendarComponent: .hour, value: 2)!
+        let hoursBetween = Date.hoursBetween(date1, date2)
+        XCTAssertEqual(hoursBetween, 2)
+    }
+    
+    func testDateMinusHoursAndHoursBetween() {
+        let date1 = Date().minus(calendarComponent: .hour, value: 2)!
+        let date2 = Date()
+        let hoursBetween = Date.hoursBetween(date1, date2)
+        XCTAssertEqual(hoursBetween, 2)
+    }
+    
+    //minutes
+    
+    func testDatePlusMinutesAndMinutesBetween() {
+        let date1 = Date()
+        let date2 = Date().plus(calendarComponent: .minute, value: 2)!
+        let minutesBetween = Date.minutesBetween(date1, date2)
+        XCTAssertEqual(minutesBetween, 2)
+    }
+    
+    func testDateMinusMinutesAndMinutesBetween() {
+        let date1 = Date().minus(calendarComponent: .minute, value: 2)!
+        let date2 = Date()
+        let minutesBetween = Date.minutesBetween(date1, date2)
+        XCTAssertEqual(minutesBetween, 2)
+    }
+    
+    //seconds
+    
+    func testDatePlusSecondsAndSecondsBetween() {
+        let date1 = Date()
+        let date2 = Date().plus(calendarComponent: .second, value: 30)!
+        let secondsBetween = Date.secondsBetween(date1, date2)
+        XCTAssertEqual(secondsBetween, 30)
+    }
+    
+    func testDateMinusSecondsAndSecondsBetween() {
+        let date1 = Date().minus(calendarComponent: .second, value: 30)!
+        let date2 = Date()
+        let secondsBetween = Date.secondsBetween(date1, date2)
+        XCTAssertEqual(secondsBetween, 30)
+    }
+
+    //MARK: - Same
+
+    func testIsSameDay() {
+        let date1 = Date()
+        let date2 = Date()
+        let date3 = Date().minus(calendarComponent: .year, value: 3)!
+
+        XCTAssertTrue(Date.isSameDay(date1: date1, date2: date2))
+        XCTAssertFalse(Date.isSameDay(date1: date1, date2: date3))
+    }
+    
+    //MARK: - Merge
+    
+    func testMerge() {
+        let calendar = Calendar.current
+        
+        let date = calendar.date(from: DateComponents(year: 2025, month: 3, day: 5))!
+        let time = calendar.date(from: DateComponents(hour: 10, minute: 30, second: 0))!
+        
+        let mergedDate = Date.merge(date: date, time: time)
+        let expectedDate = calendar.date(from: DateComponents(year: 2025, month: 3, day: 5, hour: 10, minute: 30, second: 0))!
+        
+        XCTAssertEqual(mergedDate, expectedDate)
+    }
+    
+    //MARK: - Time String
+
+//    func testTimeString10Days() {
+//        let date1 = Date()
+//        let date2 = Date().plus(calendarComponent: .day, value: 10)!
+//        
+//        let timeString = Date.timeString(date1: date1, date2: date2)
+//        let expectedTimeString = "10d"
+//        XCTAssertEqual(timeString, expectedTimeString)
+//    }
+    
+    func testTimeString1Day() {
+        let date1 = Date()
+        let date2 = Date().plus(calendarComponent: .day, value: 1)!
+        
+        let timeString = Date.timeString(date1: date1, date2: date2)
+        let expectedTimeString = "1d"
+        XCTAssertEqual(timeString, expectedTimeString)
+    }
+    
+    func testTimeString10Hours() {
+        let date1 = Date()
+        let date2 = Date().plus(calendarComponent: .hour, value: 10)!
+        
+        let timeString = Date.timeString(date1: date1, date2: date2)
+        let expectedTimeString = "10:00:00"
+        XCTAssertEqual(timeString, expectedTimeString)
+    }
+    
+    func testTimeString9Hours() {
+        let date1 = Date()
+        let date2 = Date().plus(calendarComponent: .hour, value: 9)!
+        
+        let timeString = Date.timeString(date1: date1, date2: date2)
+        let expectedTimeString = "9:00:00"
+        XCTAssertEqual(timeString, expectedTimeString)
+    }
+    
+    func testTimeString2Hours() {
+        let date1 = Date()
+        let date2 = Date().plus(calendarComponent: .hour, value: 2)!
+        
+        let timeString = Date.timeString(date1: date1, date2: date2)
+        let expectedTimeString = "2:00:00"
+        XCTAssertEqual(timeString, expectedTimeString)
+    }
+    
+    func testTimeString1Hour() {
+        let date1 = Date()
+        let date2 = Date().plus(calendarComponent: .hour, value: 1)!
+        
+        let timeString = Date.timeString(date1: date1, date2: date2)
+        let expectedTimeString = "1:00:00"
+        XCTAssertEqual(timeString, expectedTimeString)
+    }
+    
+    func testTimeString30Minutes() {
+        let date1 = Date()
+        let date2 = Date().plus(calendarComponent: .minute, value: 30)!
+        
+        let timeString = Date.timeString(date1: date1, date2: date2)
+        let expectedTimeString = "30:00"
+        XCTAssertEqual(timeString, expectedTimeString)
+    }
+    
+    func testTimeString8Seconds() {
+        let date1 = Date()
+        let date2 = Date().plus(calendarComponent: .second, value: 8)!
+        
+        let timeString = Date.timeString(date1: date1, date2: date2)
+        let expectedTimeString = "00:08"
+        XCTAssertEqual(timeString, expectedTimeString)
+    }
+}
