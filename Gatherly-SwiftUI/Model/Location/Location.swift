79548050e01086cd30ccf75670830e26be46c673
samialhamad <40968198+samialhamad@users.noreply.github.com>
2025-03-10 15:07:55 -0700

    Create Location.swift

---
+++ Gatherly-SwiftUI/Model/Location/Location.swift
@@ -0,0 +1,18 @@
+//
+//  Location.swift
+//  Gatherly-SwiftUI
+//
+//  Created by Sami Alhamad on 3/10/25.
+//
+
+import CoreLocation
+
+struct Location: Codable, Equatable, Hashable {
+    var latitude: Double
+    var longitude: Double
+    var name: String?
+    
+    var coordinate: CLLocationCoordinate2D {
+        CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+    }
+}
