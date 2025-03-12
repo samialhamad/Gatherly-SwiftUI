57327725fe7b557796bc4540e5c321f70c451fde
samialhamad <40968198+samialhamad@users.noreply.github.com>
2025-03-12 15:24:05 -0700

    Global NavigationBar styling

--- Gatherly-SwiftUI/Gatherly_SwiftUIApp.swift
+++ Gatherly-SwiftUI/Gatherly_SwiftUIApp.swift
@@ -11,6 +11,16 @@ import SwiftUI
 struct Gatherly_SwiftUIApp: App {
     @StateObject var navigationState = NavigationState()
     
+    init() {
+        let appearance = UINavigationBarAppearance()
+        appearance.configureWithOpaqueBackground()
+        appearance.backgroundColor = Brand.Colors.primary
+        appearance.titleTextAttributes = [.foregroundColor: Brand.Colors.secondary]
+        appearance.largeTitleTextAttributes = [.foregroundColor: Brand.Colors.secondary]
+        UINavigationBar.appearance().standardAppearance = appearance
+        UINavigationBar.appearance().scrollEdgeAppearance = appearance
+    }
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
