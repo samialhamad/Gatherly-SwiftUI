4b8dbbd16fd0ff267859009aac590d255d5140db
samialhamad <40968198+samialhamad@users.noreply.github.com>
2025-03-12 15:42:35 -0700

    Added navigationBarAppearance styling

--- Gatherly-SwiftUI/Gatherly_SwiftUIApp.swift
+++ Gatherly-SwiftUI/Gatherly_SwiftUIApp.swift
@@ -12,13 +12,35 @@ struct Gatherly_SwiftUIApp: App {
     @StateObject var navigationState = NavigationState()
     
     init() {
-        let appearance = UINavigationBarAppearance()
-        appearance.configureWithOpaqueBackground()
-        appearance.backgroundColor = Brand.Colors.primary
-        appearance.titleTextAttributes = [.foregroundColor: Brand.Colors.secondary]
-        appearance.largeTitleTextAttributes = [.foregroundColor: Brand.Colors.secondary]
-        UINavigationBar.appearance().standardAppearance = appearance
-        UINavigationBar.appearance().scrollEdgeAppearance = appearance
+        let navigationBarAppearance = UINavigationBarAppearance()
+        navigationBarAppearance.configureWithOpaqueBackground()
+        navigationBarAppearance.backgroundColor = Brand.Colors.primary
+        navigationBarAppearance.titleTextAttributes = [.foregroundColor: Brand.Colors.secondary]
+        navigationBarAppearance.largeTitleTextAttributes = [.foregroundColor: Brand.Colors.secondary]
+        UINavigationBar.appearance().standardAppearance = navigationBarAppearance
+        UINavigationBar.appearance().scrollEdgeAppearance = navigationBarAppearance
+        
+        let tabBarAppearance = UITabBarAppearance()
+        tabBarAppearance.configureWithOpaqueBackground()
+        tabBarAppearance.backgroundColor = Brand.Colors.primary
+        
+        //selected state
+        tabBarAppearance.stackedLayoutAppearance.selected.iconColor = Brand.Colors.secondary
+        tabBarAppearance.stackedLayoutAppearance.selected.titleTextAttributes = [
+            .foregroundColor: Brand.Colors.secondary
+        ]
+        
+        //unselected state
+        tabBarAppearance.stackedLayoutAppearance.normal.iconColor = Brand.Colors.tertiary
+        tabBarAppearance.stackedLayoutAppearance.normal.titleTextAttributes = [
+            .foregroundColor: Brand.Colors.tertiary
+        ]
+        
+        UITabBar.appearance().standardAppearance = tabBarAppearance
+        
+        if #available(iOS 15.0, *) {
+            UITabBar.appearance().scrollEdgeAppearance = tabBarAppearance
+        }
     }
     
     var body: some Scene {
