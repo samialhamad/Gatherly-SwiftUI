674bf316a3d9e3a4193f555a78e5fd580f600ef5
samialhamad <40968198+samialhamad@users.noreply.github.com>
2025-03-12 15:23:55 -0700

    Create Gatherly.swift

---
+++ Gatherly-SwiftUI/Brand/Gatherly.swift
@@ -0,0 +1,26 @@
+//
+//  Gatherly.swift
+//  Gatherly-SwiftUI
+//
+//  Created by Sami Alhamad on 3/12/25.
+//
+
+import UIKit
+
+struct Brand {
+    
+    //MARK: - Colors
+
+    struct Colors {
+        static var primary: UIColor {
+            return UIColor(red: 121.0 / 255.0, green: 147.0 / 255.0, blue: 252.0 / 255.0, alpha: 1.0)
+        }
+        static var secondary: UIColor {
+            return UIColor(red: 230.0 / 255.0, green: 234.0 / 255.0, blue: 252.0 / 255.0, alpha: 1.0)
+        }
+        static var tertiary: UIColor {
+            return UIColor(red: 41.0 / 255.0, green: 42.0 / 255.0, blue: 44.0 / 255.0, alpha: 1.0)
+        }
+    }
+}
+
