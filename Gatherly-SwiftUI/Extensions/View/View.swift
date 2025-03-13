924b5e8fc822b77a58fb6098186b86952f83bde1
samialhamad <40968198+samialhamad@users.noreply.github.com>
2025-03-13 14:19:57 -0700

    View extension, with toolbar styling

---
+++ Gatherly-SwiftUI/Extensions/View/View.swift
@@ -0,0 +1,18 @@
+//
+//  View.swift
+//  Gatherly-SwiftUI
+//
+//  Created by Sami Alhamad on 3/13/25.
+//
+
+import SwiftUI
+
+extension View {
+    func applyGatherlyToolbarStyle() -> some View {
+        self.toolbarColor(Color(Brand.Colors.secondary))
+    }
+
+    private func toolbarColor(_ color: Color) -> some View {
+        self.tint(color)
+    }
+}
