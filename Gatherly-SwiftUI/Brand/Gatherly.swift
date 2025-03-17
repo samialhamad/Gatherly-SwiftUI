e99670b3c8ff6d017516422745acf79461a0df08
samialhamad <40968198+samialhamad@users.noreply.github.com>
2025-03-17 14:52:48 -0700

    Modified accent color

--- Gatherly-SwiftUI/Brand/Gatherly.swift
+++ Gatherly-SwiftUI/Brand/Gatherly.swift
@@ -19,11 +19,10 @@ struct Brand {
             return UIColor(red: 230.0 / 255.0, green: 234.0 / 255.0, blue: 252.0 / 255.0, alpha: 1.0)
         }
         static var accent: UIColor {
-            return UIColor(red: 120.0 / 255.0, green: 81.0 / 255.0, blue: 169.0 / 255.0, alpha: 1.0)
+            return UIColor(red: 102.0 / 255.0, green: 110.0 / 255.0, blue: 120.0 / 255.0, alpha: 1.0)
         }
         static var tertiary: UIColor {
             return UIColor(red: 41.0 / 255.0, green: 42.0 / 255.0, blue: 44.0 / 255.0, alpha: 1.0)
         }
     }
 }
-
