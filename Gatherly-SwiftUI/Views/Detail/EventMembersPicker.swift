6e39a85d88275073cf7495c070aac1b8840c499e
samialhamad <40968198+samialhamad@users.noreply.github.com>
2025-03-18 19:05:24 -0700

    Create EventMembersPicker.swift

---
+++ Gatherly-SwiftUI/Views/Detail/EventMembersPicker.swift
@@ -0,0 +1,53 @@
+//
+//  EventMembersPicker.swift
+//  Gatherly-SwiftUI
+//
+//  Created by Sami Alhamad on 3/18/25.
+//
+
+import SwiftUI
+
+struct EventMembersPicker: View {
+    let allUsers: [User]
+    @Binding var selectedMemberIDs: Set<Int>
+    @Environment(\.dismiss) var dismiss
+
+    var body: some View {
+        NavigationStack {
+            List(allUsers, id: \.id) { user in
+                Button(action: {
+                    toggleMemberSelection(user.id)
+                }) {
+                    HStack {
+                        Text("\(user.firstName ?? "") \(user.lastName ?? "")")
+                            .foregroundColor(.primary)
+                        Spacer()
+                        if selectedMemberIDs.contains(user.id ?? -1) {
+                            Image(systemName: "checkmark")
+                                .foregroundColor(Color(Brand.Colors.primary))
+                        }
+                    }
+                    .contentShape(Rectangle())
+                }
+                .listRowSeparator(.hidden)
+            }
+            .navigationTitle("Select Members")
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button("Done") {
+                        dismiss()
+                    }
+                }
+            }
+        }
+    }
+    
+    private func toggleMemberSelection(_ id: Int?) {
+        guard let id = id else { return }
+        if selectedMemberIDs.contains(id) {
+            selectedMemberIDs.remove(id)
+        } else {
+            selectedMemberIDs.insert(id)
+        }
+    }
+}
