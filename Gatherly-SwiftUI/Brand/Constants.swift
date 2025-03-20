102f7e2fa4f588ba68f9e06a9654c3205a6f5333
samialhamad <40968198+samialhamad@users.noreply.github.com>
2025-03-20 16:58:57 -0700

    Added Constants.EventDetailView

--- Gatherly-SwiftUI/Brand/Constants.swift
+++ Gatherly-SwiftUI/Brand/Constants.swift
@@ -16,12 +16,20 @@ struct Constants {
         static let zeroSpacing: CGFloat = 0
     }
 
+    struct EventDetailView {
+        static let bodyVStackSpacing: CGFloat = 16
+        static let eventMapPreviewFrame: CGFloat = 200
+        static let eventMapPreviewVStackSpacing: CGFloat = 2
+        static let eventMapPreviewTextPadding: CGFloat = 4
+        static let eventMapPreviewCornerRadius: CGFloat = 8
+        static let eventMapPreviewShadow: CGFloat = 2
+    }
+    
     struct EventsGroupedListView {
         static let topFrameHeight: CGFloat = 20
     }
     
-    struct CategoryListView {
+    struct EventMembersPicker {
         static let topPadding: CGFloat = 20
-        static let bottomPadding: CGFloat = 20
     }
 }
