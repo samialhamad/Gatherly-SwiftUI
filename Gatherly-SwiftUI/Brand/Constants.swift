22b07d4613ebb002e6c1b0ffa2aa7941a226990d
samialhamad <40968198+samialhamad@users.noreply.github.com>
2025-03-20 16:47:37 -0700

    Create Constants.swift

---
+++ Gatherly-SwiftUI/Brand/Constants.swift
@@ -0,0 +1,22 @@
+//
+//  Constants.swift
+//  Gatherly-SwiftUI
+//
+//  Created by Sami Alhamad on 3/20/25.
+//
+
+// Constants.swift
+
+import SwiftUI
+
+struct Constants {
+    struct CalendarView {
+        static let eventListViewSpacing: CGFloat = 10
+        static let headerViewSpacing: CGFloat = 4
+        static let zeroSpacing: CGFloat = 0
+    }
+
+    struct EventsGroupedListView {
+        static let topFrameHeight: CGFloat = 20
+    }
+}
