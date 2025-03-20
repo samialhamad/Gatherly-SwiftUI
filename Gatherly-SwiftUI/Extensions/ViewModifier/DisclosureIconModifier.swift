a7c3af5147c26496d9a02b1fa0a1744b496e3fce
samialhamad <40968198+samialhamad@users.noreply.github.com>
2025-03-19 18:22:56 -0700

    Create DisclosureIconModifier.swift

---
+++ Gatherly-SwiftUI/Extensions/ViewModifier/DisclosureIconModifier.swift
@@ -0,0 +1,18 @@
+//
+//  DisclosureIconModifier.swift
+//  Gatherly-SwiftUI
+//
+//  Created by Sami Alhamad on 3/19/25.
+//
+
+import SwiftUI
+
+struct DisclosureIconModifier: ViewModifier {
+    func body(content: Content) -> some View {
+        HStack {
+            content
+            Image(systemName: "chevron.right")
+                .foregroundColor(Color(Brand.Colors.primary))
+        }
+    }
+}
