7d1c4d49c3c1559360852ecb5c46d7e851838c0d
samialhamad <40968198+samialhamad@users.noreply.github.com>
2025-03-24 15:12:28 -0700

    Create AlphabetIndexView.swift

---
+++ Gatherly-SwiftUI/Views/Components/AlphabetIndexView.swift
@@ -0,0 +1,34 @@
+//
+//  AlphabetIndexView.swift
+//  Gatherly-SwiftUI
+//
+//  Created by Sami Alhamad on 3/24/25.
+//
+
+import SwiftUI
+
+struct AlphabetIndexView: View {
+    let letters: [String]
+    var onLetterTap: (String) -> Void
+
+    var body: some View {
+        VStack {
+            ForEach(letters, id: \.self) { letter in
+                Text(letter)
+                    .font(.caption)
+                    .padding(2)
+                    .onTapGesture {
+                        onLetterTap(letter)
+                    }
+            }
+        }
+        .background(Color(.systemBackground).opacity(0.7))
+        .cornerRadius(8)
+    }
+}
+
+#Preview {
+    AlphabetIndexView(letters: ["A", "B", "C", "D"]) { letter in
+        print("Tapped letter: \(letter)")
+    }
+}
