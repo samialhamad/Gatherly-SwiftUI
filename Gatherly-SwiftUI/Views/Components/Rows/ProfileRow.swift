beab527b26a94e7fc3f5221eda6868aa3ed13f14
samialhamad <40968198+samialhamad@users.noreply.github.com>
2025-03-25 20:27:09 -0700

    Update ProfileRow to use AvatarView

--- Gatherly-SwiftUI/Views/Components/Rows/ProfileRow.swift
+++ Gatherly-SwiftUI/Views/Components/Rows/ProfileRow.swift
@@ -9,24 +9,15 @@ import SwiftUI
 
 struct ProfileRow: View {
     let user: User
-
-    var initials: String {
-        let firstInitial = user.firstName?.first.map(String.init) ?? ""
-        let lastInitial = user.lastName?.first.map(String.init) ?? ""
-        return firstInitial + lastInitial
-    }
-    
+        
     var body: some View {
         HStack {
-            //avatar placeholder - user initial
-            Circle()
-                .fill(Color(Colors.primary))
-                .frame(width: Constants.ProfileRow.avatarCircleWidth, height: Constants.ProfileRow.avatarCircleHeight)
-                .overlay(
-                    Text(initials)
-                        .font(.headline)
-                        .foregroundColor(.white)
-                )
+            AvatarView(
+                user: user,
+                size: Constants.ProfileRow.avatarCircleSize,
+                font: .headline,
+                backgroundColor: Color(Colors.primary)
+            )
             VStack(alignment: .leading) {
                 Text("\(user.firstName ?? "") \(user.lastName ?? "")")
                     .font(.body)
