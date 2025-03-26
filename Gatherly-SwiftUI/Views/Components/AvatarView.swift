11998bc3ca85c22d8605d2d246f63921fa8a62d1
samialhamad <40968198+samialhamad@users.noreply.github.com>
2025-03-25 20:21:27 -0700

    Create AvatarView.swift

---
+++ Gatherly-SwiftUI/Views/Components/AvatarView.swift
@@ -0,0 +1,41 @@
+//
+//  AvatarView.swift
+//  Gatherly-SwiftUI
+//
+//  Created by Sami Alhamad on 3/25/25.
+//
+
+import SwiftUI
+
+struct AvatarView: View {
+    let user: User
+    let size: CGFloat
+    let font: Font
+    let backgroundColor: Color
+    let borderColor: Color?
+    let borderWidth: CGFloat?
+    
+    private var initials: String {
+        let firstInitial = user.firstName?.first.map(String.init) ?? ""
+        let lastInitial = user.lastName?.first.map(String.init) ?? ""
+        return firstInitial + lastInitial
+    }
+    
+    var body: some View {
+        Circle()
+            .fill(backgroundColor)
+            .frame(width: size, height: size)
+            .overlay(
+                Text(initials)
+                    .font(font)
+                    .foregroundColor(.white)
+            )
+            .overlay(
+                Group {
+                    if let borderColor = borderColor, let borderWidth = borderWidth {
+                        Circle().stroke(borderColor, lineWidth: borderWidth)
+                    }
+                }
+            )
+    }
+}
