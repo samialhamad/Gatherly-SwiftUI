0f2e49379392f38d9197f34345d947897ce89e2e
samialhamad <40968198+samialhamad@users.noreply.github.com>
2025-03-31 14:23:22 -0700

    Update SampleData.swift

--- Gatherly-SwiftUI/SampleData.swift
+++ Gatherly-SwiftUI/SampleData.swift
@@ -117,6 +117,30 @@ struct SampleData {
             isEmailEnabled: true,
             lastName: "Zebrus",
             phone: "9876543210"
+        ),
+        User(
+            createdTimestamp: Int(Date().timestamp),
+            deviceToken: "45596",
+            email: "woah@zowers.com",
+            eventIDs: nil,
+            firstName: "LeBron",
+            friendIDs: nil,
+            id: 10,
+            isEmailEnabled: true,
+            lastName: "James",
+            phone: "122872722"
+        ),
+        User(
+            createdTimestamp: Int(Date().timestamp),
+            deviceToken: "mj232",
+            email: "mj@nike.com",
+            eventIDs: nil,
+            firstName: "Michael",
+            friendIDs: nil,
+            id: 11,
+            isEmailEnabled: true,
+            lastName: "Jordan",
+            phone: "9876543210"
         )
     ]
     
