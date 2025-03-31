b03572a151a8bb36cab4215a051fc97c50c8d933
samialhamad <40968198+samialhamad@users.noreply.github.com>
2025-03-31 15:05:39 -0700

    Create AddFriendView

--- Gatherly-SwiftUI/Views/Tabs/FriendsView/Subviews/AddFriendView.swift
+++ Gatherly-SwiftUI/Views/Tabs/FriendsView/Subviews/AddFriendView.swift
@@ -5,3 +5,41 @@
 //  Created by Sami Alhamad on 3/24/25.
 //
 
+import SwiftUI
+
+struct AddFriendView: View {
+    @Environment(\.dismiss) private var dismiss
+    @StateObject var viewModel: AddFriendViewModel
+    
+    var body: some View {
+        NavigationStack {
+            VStack {
+                SearchBarView(searchText: $viewModel.searchText, placeholder: "Search by name")
+                
+                if !viewModel.searchText.trimmingCharacters(in: .whitespaces).isEmpty {
+                    List(viewModel.filteredUsers, id: \.id) { user in
+                        HStack {
+                            ProfileRow(user: user)
+                            Spacer()
+                            Button("Add") {
+                                viewModel.sendFriendRequest(to: user)
+                            }
+                            .buttonStyle(.borderedProminent)
+                        }
+                    }
+                    .listStyle(.plain)
+                } else {
+                    Spacer()
+                }
+            }
+            .navigationTitle("Add Friend")
+            .toolbar {
+                ToolbarItem(placement: .cancellationAction) {
+                    Button("Close") {
+                        dismiss()
+                    }
+                }
+            }
+        }
+    }
+}
