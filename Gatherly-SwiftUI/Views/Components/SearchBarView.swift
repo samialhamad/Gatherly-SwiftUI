d781d8243eb93404004b513f4e1864f55cfca723
samialhamad <40968198+samialhamad@users.noreply.github.com>
2025-03-31 14:34:14 -0700

    Create SearchBarView.swift

---
+++ Gatherly-SwiftUI/Views/Components/SearchBarView.swift
@@ -0,0 +1,23 @@
+//
+//  SearchBarView.swift
+//  Gatherly-SwiftUI
+//
+//  Created by Sami Alhamad on 3/31/25.
+//
+
+import SwiftUI
+
+struct SearchBarView: View {
+    @Binding var searchText: String
+    var placeholder: String = "Search"
+    
+    var body: some View {
+        HStack {
+            Image(systemName: "magnifyingglass")
+                .foregroundColor(Color(Colors.primary))
+            TextField(placeholder, text: $searchText)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+        }
+        .padding()
+    }
+}
