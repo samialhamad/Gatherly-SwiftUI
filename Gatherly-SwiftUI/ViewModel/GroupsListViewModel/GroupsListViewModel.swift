2a738375bf2d93ff64a27547360582d2780b6490
samialhamad <40968198+samialhamad@users.noreply.github.com>
2025-03-31 13:43:48 -0700

    Refactor GroupsListViewModel

--- Gatherly-SwiftUI/ViewModel/GroupsListViewModel/GroupsListViewModel.swift
+++ Gatherly-SwiftUI/ViewModel/GroupsListViewModel/GroupsListViewModel.swift
@@ -8,22 +8,36 @@
 import Foundation
 
 class GroupsListViewModel: ObservableObject {
-    private var currentUser: User = SampleData.sampleUsers.first { $0.id == 1 }! // replace in future, right now hard coded to user id 1
+    private var currentUserID: Int
+    private let allUsers: [User]
+    private var allUserGroups: [UserGroup]
     
     @Published var searchText: String = ""
     
+    //needs to change in future
+    init(currentUserID: Int = 1,
+         allUsers: [User] = SampleData.sampleUsers,
+         allUserGroups: [UserGroup] = SampleData.sampleGroups) {
+        self.currentUserID = currentUserID
+        self.allUsers = allUsers
+        self.allUserGroups = allUserGroups
+    }
+    
     // also needs to be revisited once sampleData is done away with
-    var allGroups: [UserGroup] {
-           guard let userGroupIDs = currentUser.groupIDs else { return [] }
-           return SampleData.sampleGroups.filter { userGroupIDs.contains($0.id) }
-       }
+    var groups: [UserGroup] {
+        guard let currentUser = allUsers.first(where: { $0.id == currentUserID }),
+            let userGroupIDs = currentUser.groupIDs else {
+            return []
+        }
+        return SampleData.sampleGroups.filter { userGroupIDs.contains($0.id) }
+    }
     
     var filteredGroups: [UserGroup] {
         if searchText.isEmpty {
-            return allGroups
+            return groups
         } else {
             let lowercasedQuery = searchText.lowercased()
-            return allGroups.filter {
+            return groups.filter {
                 $0.name.lowercased().contains(lowercasedQuery)
             }
         }
