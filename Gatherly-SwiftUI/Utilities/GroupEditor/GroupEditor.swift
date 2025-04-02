02ba5192e58d9fdda730435c5d153d6a1835d6fc
samialhamad <40968198+samialhamad@users.noreply.github.com>
2025-04-01 17:01:00 -0700

    Create GroupEditor.swift

---
+++ Gatherly-SwiftUI/Utilities/GroupEditor/GroupEditor.swift
@@ -0,0 +1,48 @@
+//
+//  GroupEditor.swift
+//  Gatherly-SwiftUI
+//
+//  Created by Sami Alhamad on 4/1/25.
+//
+
+import Foundation
+import SwiftUI
+
+struct GroupEditor {
+    
+    // MARK: - Save / Edit
+    
+    static func saveGroup(
+        originalGroup: UserGroup? = nil,
+        name: String,
+        memberIDs: Set<Int>,
+        imageName: String? = nil,
+        bannerImageName: String? = nil,
+        creatorID: Int,
+        generateGroupID: () -> Int = { Int.random(in: 10000...99999) }
+    ) -> UserGroup {
+        
+        let groupID = originalGroup?.id ?? generateGroupID()
+        let leaderID = originalGroup?.leaderID ?? creatorID
+        
+        var group = UserGroup(
+            id: groupID,
+            name: name,
+            memberIDs: Array(memberIDs).sorted(),
+            leaderID: leaderID,
+            messages: originalGroup?.messages
+        )
+        
+        group.imageName = imageName
+        group.bannerImageName = bannerImageName
+        
+        return group
+    }
+    
+    // MARK: - Delete
+    
+    static func deleteGroup(from groups: [UserGroup], groupToDelete: UserGroup) -> [UserGroup] {
+        groups.filter { $0.id != groupToDelete.id }
+    }
+}
+
