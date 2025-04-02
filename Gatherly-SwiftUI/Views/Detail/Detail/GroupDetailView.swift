b2f6288ba4fa89412839f34c3c410f3d41a79140
samialhamad <40968198+samialhamad@users.noreply.github.com>
2025-04-02 12:26:08 -0700

    Display profile and banner image on GroupDetailView

--- Gatherly-SwiftUI/Views/Detail/Detail/GroupDetailView.swift
+++ Gatherly-SwiftUI/Views/Detail/Detail/GroupDetailView.swift
@@ -13,7 +13,7 @@ struct GroupDetailView: View {
     var body: some View {
         ScrollView {
             VStack {
-                AvatarHeaderView(group: group)
+                AvatarHeaderView(group: group, profileImage: profileImage, bannerImage: bannerImage)
                 
                 VStack(spacing: 8) {
                     Text("Leader ID: \(group.leaderID)")
@@ -29,6 +29,24 @@ struct GroupDetailView: View {
             }
         }
         .navigationTitle(group.name)
+    }
+}
+
+private extension GroupDetailView {
+    var profileImage: UIImage? {
+        guard let imageName = group.imageName else {
+            return nil
+        }
+        
+        return ImageUtility.loadImageFromDocuments(named: imageName)
+    }
+
+    var bannerImage: UIImage? {
+        guard let bannerName = group.bannerImageName else {
+            return nil
+        }
+        
+        return ImageUtility.loadImageFromDocuments(named: bannerName)
     }
 }
 
