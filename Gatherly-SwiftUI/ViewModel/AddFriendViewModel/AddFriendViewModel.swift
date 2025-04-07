22c7f92a44649327eaa25b55dcd8985cc48b80ee
samialhamad <40968198+samialhamad@users.noreply.github.com>
2025-04-07 15:53:25 -0700

    Update ViewModel to use ContactSyncManager

--- Gatherly-SwiftUI/ViewModel/AddFriendViewModel/AddFriendViewModel.swift
+++ Gatherly-SwiftUI/ViewModel/AddFriendViewModel/AddFriendViewModel.swift
@@ -7,21 +7,39 @@
 
 import Foundation
 import SwiftUI
+import Contacts
 
 class AddFriendViewModel: ObservableObject {
     @Published var searchText: String = ""
     @Published var allUsers: [User]
     @Published var currentUser: User
+    @Published var matchedContacts: [User] = []
+    @Published var didSyncContacts = false
     
     init(currentUser: User, allUsers: [User]) {
         self.currentUser = currentUser
         self.allUsers = allUsers
     }
     
+    // MARK: - Sync Contacts
+    func syncContacts() {
+        ContactSyncManager.shared.fetchContactsPhoneNumbers { contactNumbers in
+            self.matchedContacts = self.allUsers.filter { user in
+                guard let phone = user.phone else { return false }
+                let cleaned = phone.filter(\.isWholeNumber)
+                return contactNumbers.contains(cleaned)
+            }
+            self.didSyncContacts = true
+        }
+    }
+    
+    // MARK: - Filtered Results
+
     var filteredUsers: [User] {
         let query = searchText.trimmingCharacters(in: .whitespacesAndNewlines).lowercased()
+        let usersToSearch = didSyncContacts ? matchedContacts : allUsers
         
-        return allUsers.filter { user in
+        return usersToSearch.filter { user in
             guard let currentID = currentUser.id else { return false }
             
             let isNotSelf = user.id != currentID
