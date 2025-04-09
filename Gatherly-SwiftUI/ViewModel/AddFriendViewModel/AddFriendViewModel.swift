1dc1d840dba96f1215c5c9ac20731ab052044aae
samialhamad <40968198+samialhamad@users.noreply.github.com>
2025-04-09 15:56:29 -0700

    Update init() for AddFriendVM

--- Gatherly-SwiftUI/ViewModel/AddFriendViewModel/AddFriendViewModel.swift
+++ Gatherly-SwiftUI/ViewModel/AddFriendViewModel/AddFriendViewModel.swift
@@ -17,24 +17,18 @@ class AddFriendViewModel: ObservableObject {
     @Published var didSyncContacts = false
     @Published var syncedContacts: [SyncedContact] = []
     
-    init(currentUser: User, allUsers: [User]) {
+    init(currentUser: User, allUsers: [User], syncedContacts: [SyncedContact]) {
         self.currentUser = currentUser
         self.allUsers = allUsers
-    }
-    
-    func syncContacts() {
-        ContactSyncManager.shared.fetchContacts { contacts in
-            self.syncedContacts = contacts
-            
-            let contactPhones = Set(contacts.map { $0.phoneNumber })
-            
-            self.matchedContacts = self.allUsers.filter { user in
-                guard let phone = user.phone else { return false }
-                let cleaned = phone.filter(\.isWholeNumber)
-                return contactPhones.contains(cleaned)
-            }
-            
-            self.didSyncContacts = true
+        self.syncedContacts = syncedContacts
+        self.didSyncContacts = !syncedContacts.isEmpty
+        
+        // update once backend implemented
+        let contactPhones = Set(syncedContacts.map { $0.phoneNumber })
+        self.matchedContacts = allUsers.filter { user in
+            guard let phone = user.phone else { return false }
+            let cleaned = phone.filter(\.isWholeNumber)
+            return contactPhones.contains(cleaned)
         }
     }
     
@@ -42,7 +36,7 @@ class AddFriendViewModel: ObservableObject {
         guard let phone = user.phone?.filter(\.isWholeNumber) else { return nil }
         return syncedContacts.first(where: { $0.phoneNumber == phone })?.fullName
     }
-
+    
     var filteredUsers: [User] {
         let query = searchText.trimmingCharacters(in: .whitespacesAndNewlines).lowercased()
         let usersToSearch = didSyncContacts ? matchedContacts : allUsers
