2cad0d71ab55cf0fab578edd8af3b5d4b4fc86d6
samialhamad <40968198+samialhamad@users.noreply.github.com>
2025-04-10 13:18:20 -0700

    Added Constants eventMapPreviewButtonPadding

--- Gatherly-SwiftUI/Brand/Constants.swift
+++ Gatherly-SwiftUI/Brand/Constants.swift
@@ -65,6 +65,7 @@ struct Constants {
     struct EventDetailView {
         static let bodyVStackSpacing: CGFloat = 16
         static let eventCategoriesViewSpacing: CGFloat = 8
+        static let eventMapPreviewButtonPadding: CGFloat = 4
         static let eventMapPreviewFrame: CGFloat = 200
         static let eventMapPreviewVStackSpacing: CGFloat = 2
         static let eventMapPreviewTextPadding: CGFloat = 4
