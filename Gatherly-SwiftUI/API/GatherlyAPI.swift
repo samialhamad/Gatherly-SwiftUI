cd3fda84c7811a34b2a5f29e08315e84dbef4950
samialhamad <40968198+samialhamad@users.noreply.github.com>
2025-04-14 13:01:39 -0700

    Create GatherlyAPI.swift

---
+++ Gatherly-SwiftUI/API/GatherlyAPI.swift
@@ -0,0 +1,13 @@
+//
+//  GatherlyAPI.swift
+//  Gatherly-SwiftUI
+//
+//  Created by Sami Alhamad on 4/14/25.
+//
+
+import Foundation
+import Combine
+
+final class GatherlyAPI {
+    // namespace only — actual logic lives in extensions in subfolders
+}
