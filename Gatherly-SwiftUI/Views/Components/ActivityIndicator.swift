fb0ec436d11bcce779f58dd9b25e59d970f8a364
samialhamad <40968198+samialhamad@users.noreply.github.com>
2025-04-14 14:08:52 -0700

    Updated ActivityIndicator UI

--- Gatherly-SwiftUI/Views/Components/ActivityIndicator.swift
+++ Gatherly-SwiftUI/Views/Components/ActivityIndicator.swift
@@ -8,12 +8,22 @@
 import SwiftUI
 
 struct ActivityIndicator: View {
+    var message: String? = nil
+    
     var body: some View {
-        ProgressView()
-            .progressViewStyle(CircularProgressViewStyle())
-            .scaleEffect(1.5)
-            .frame(maxWidth: .infinity, maxHeight: .infinity)
-            .background(Color.black.opacity(0.3))
-            .edgesIgnoringSafeArea(.all)
+        VStack(spacing: Constants.ActivityIndicator.vstackSpacing) {
+            ProgressView()
+                .progressViewStyle(CircularProgressViewStyle(tint: Color(Colors.primary)))
+                .scaleEffect(Constants.ActivityIndicator.scaleEffect)
+            
+            if let message = message {
+                Text(message)
+                    .font(.body)
+                    .foregroundColor(Color(Colors.primary))
+            }
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(Color(Colors.secondary))
+        .edgesIgnoringSafeArea(.all)
     }
 }
