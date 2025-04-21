9bae7ede75abe16d16601c628a8dd7f76516b6b9
samialhamad <40968198+samialhamad@users.noreply.github.com>
2025-04-21 13:30:54 -0700

    Update CreateGroupView to handle existingGroups

--- Gatherly-SwiftUI/Views/Tabs/FriendsView/Subviews/CreateGroupView.swift
+++ Gatherly-SwiftUI/Views/Tabs/FriendsView/Subviews/CreateGroupView.swift
@@ -77,7 +77,7 @@ private extension CreateGroupView {
     var createButtonSection: some View {
         Section {
             Button(action: {
-                let newGroup = viewModel.createGroup(leaderID: 1)
+                let newGroup = viewModel.createGroup(leaderID: currentUser.id ?? 1, existingGroups: groups)
                 groups.append(newGroup)
                 UserDefaultsManager.saveGroups(groups)
                 dismiss()
