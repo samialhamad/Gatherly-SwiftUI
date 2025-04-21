fccb04e7d1a8fe6512a8e5cbe13a1ba1b9a344ef
samialhamad <40968198+samialhamad@users.noreply.github.com>
2025-04-21 15:08:03 -0700

    Create EditProfileView.swift

---
+++ Gatherly-SwiftUI/Views/Tabs/ProfileView/Subviews/EditProfile/EditProfileView.swift
@@ -0,0 +1,54 @@
+//
+//  EditProfileView.swift
+//  Gatherly-SwiftUI
+//
+//  Created by Sami Alhamad on 4/21/25.
+//
+
+import ComposableArchitecture
+import SwiftUI
+
+struct EditProfileView: View {
+    let store: Store<EditProfileFeature.State, EditProfileFeature.Action>
+    
+    var body: some View {
+        WithViewStore(store, observe: { $0 }) { viewStore in
+            NavigationStack {
+                Form {
+                    Section(header: Text("Name")) {
+                        TextField("First Name", text: viewStore.binding(
+                            get: \.firstName,
+                            send: EditProfileFeature.Action.setFirstName
+                        ))
+                        TextField("Last Name", text: viewStore.binding(
+                            get: \.lastName,
+                            send: EditProfileFeature.Action.setLastName
+                        ))
+                    }
+                    
+                    saveAndCancelSection(viewStore: viewStore)
+                }
+                .navigationTitle("Edit Profile")
+                .toolbar {
+                    ToolbarItem(placement: .navigationBarLeading) {
+                        Button("Cancel") {
+                            viewStore.send(.cancel)
+                        }
+                    }
+                }
+            }
+            .keyboardDismissable()
+        }
+    }
+    
+    @ViewBuilder
+    private func saveAndCancelSection(viewStore: ViewStore<EditProfileFeature.State, EditProfileFeature.Action>) -> some View {
+        Section {
+            Button("Save") {
+                viewStore.send(.saveChanges)
+            }
+            .foregroundColor(Color(Colors.primary))
+        }
+    }
+}
+
