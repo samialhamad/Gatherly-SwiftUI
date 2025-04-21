368ebc9b307c0ab99e72f227bb06ce54a37b1e5c
samialhamad <40968198+samialhamad@users.noreply.github.com>
2025-04-21 13:29:31 -0700

    Update CreateGroupVM to accept existingGroups

--- Gatherly-SwiftUI/ViewModel/CreateGroupViewModel/CreateGroupViewModelTests.swift
+++ Gatherly-SwiftUI/ViewModel/CreateGroupViewModel/CreateGroupViewModelTests.swift
@@ -21,11 +21,17 @@ final class CreateGroupViewModelTests: XCTestCase {
         viewModel.bannerImage = testBannerImage
         
         let leaderID = 1
-        let group = viewModel.createGroup(leaderID: leaderID)
+        let existingGroups = [
+            UserGroup(id: 1, name: "Existing Group 1", memberIDs: [2], leaderID: 1),
+            UserGroup(id: 2, name: "Existing Group 2", memberIDs: [3], leaderID: 1)
+        ]
+        
+        let group = viewModel.createGroup(leaderID: leaderID, existingGroups: existingGroups)
         
         XCTAssertEqual(group.name, "Test Group")
         XCTAssertEqual(group.leaderID, leaderID)
         XCTAssertEqual(group.memberIDs, [2, 3])
+        XCTAssertEqual(group.id, 3)
         XCTAssertNotNil(group.imageName)
         XCTAssertNotNil(group.bannerImageName)
     }
