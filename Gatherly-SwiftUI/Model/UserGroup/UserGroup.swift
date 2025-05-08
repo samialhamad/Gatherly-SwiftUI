d28fd19e8855b4d1bc5b4a0be59777595694f9b1
samialhamad <40968198+samialhamad@users.noreply.github.com>
2025-05-08 15:27:46 -0700

    Add optional to UserGroup variables

--- Gatherly-SwiftUI/Model/UserGroup/UserGroup.swift
+++ Gatherly-SwiftUI/Model/UserGroup/UserGroup.swift
@@ -9,12 +9,12 @@ import Foundation
 
 struct UserGroup: Codable, Identifiable, Equatable {
     var bannerImageName: String? = nil
-    var id: Int
+    var id: Int?
     var imageName: String? = nil
     var leaderID: Int
     var memberIDs: [Int]
     var messages: [Message]?
-    var name: String
+    var name: String?
 
     // MARK: - Computed Vars
     
