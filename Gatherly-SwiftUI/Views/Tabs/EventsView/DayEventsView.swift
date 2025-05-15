55c34aa8a9bbd065a710a2d797b2932375853ff9
samialhamad <40968198+samialhamad@users.noreply.github.com>
2025-05-15 12:42:42 -0700

    Create DayEventsView.swift

---
+++ Gatherly-SwiftUI/Views/Tabs/EventsView/DayEventsView.swift
@@ -0,0 +1,45 @@
+//
+//  DayEventsView.swift
+//  Gatherly-SwiftUI
+//
+//  Created by Sami Alhamad on 5/15/25.
+//
+
+import SwiftUI
+
+struct DayEventsView: View {
+    @EnvironmentObject var session: AppSession
+    
+    let date: Date
+    
+    var body: some View {
+        List {
+            if eventsForDate.isEmpty {
+                Text("Nothing planned for this day!")
+                    .foregroundColor(.gray)
+            } else {
+                ForEach(eventsForDate) { event in
+                    EventRowLink(event: event)
+                }
+            }
+        }
+        .navigationTitle(date.formatted(date: .long, time: .omitted))
+    }
+}
+
+private extension DayEventsView {
+    // MARK: - Computed Vars
+    
+    private var eventsForDate: [Event] {
+        session.events
+            .filter { event in
+                guard let eventDate = event.date else {
+                    return false
+                }
+                
+                return Calendar.current.isDate(eventDate, inSameDayAs: date)
+            }
+        
+            .sorted(by: { ($0.startTimestamp ?? 0) < ($1.startTimestamp ?? 0) })
+    }
+}
