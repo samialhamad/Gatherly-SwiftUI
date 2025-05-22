6d39afa8b8ddaca0313f5a1c2ec28ebebc4b87e0
samialhamad <40968198+samialhamad@users.noreply.github.com>
2025-05-22 14:58:55 -0700

    Update UserDefaultsManager to include new flags

--- Gatherly-SwiftUI/Utilities/UserDefaultsManager/UserDefaultsManager.swift
+++ Gatherly-SwiftUI/Utilities/UserDefaultsManager/UserDefaultsManager.swift
@@ -12,6 +12,8 @@ enum UserDefaultsManager {
     // MARK: - Keys
     
     private enum Keys {
+        static let didSeedSampleData = "gatherly_didSeedSampleData"
+        static let didSyncContacts = "gatherly_didSyncContacts"
         static let events = "gatherly_events"
         static let groups = "gatherly_groups"
         static let users = "gatherly_users"
@@ -60,6 +62,29 @@ enum UserDefaultsManager {
     static func removeGroups() {
         defaults.removeObject(forKey: Keys.groups)
     }
+    
+    // MARK: - Flags
+
+    static func setDidSeedSampleData(_ value: Bool) {
+        defaults.set(value, forKey: Keys.didSeedSampleData)
+    }
+
+    static func getDidSeedSampleData() -> Bool {
+        defaults.bool(forKey: Keys.didSeedSampleData)
+    }
+
+    static func setDidSyncContacts(_ value: Bool) {
+        defaults.set(value, forKey: Keys.didSyncContacts)
+    }
+
+    static func getDidSyncContacts() -> Bool {
+        defaults.bool(forKey: Keys.didSyncContacts)
+    }
+
+    static func resetFlags() {
+        defaults.removeObject(forKey: Keys.didSeedSampleData)
+        defaults.removeObject(forKey: Keys.didSyncContacts)
+    }
 
     // MARK: - Reset
 
@@ -67,5 +92,6 @@ enum UserDefaultsManager {
         removeUsers()
         removeEvents()
         removeGroups()
+        resetFlags()
     }
 }
