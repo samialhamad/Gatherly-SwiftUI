c76dba82579522788127d21cbf2fb10a7e4ae260
samialhamad <40968198+samialhamad@users.noreply.github.com>
2025-05-22 17:57:12 -0700

    Remove GatherlyAPI.simulateNetworkDelay()

--- Gatherly-SwiftUI/API/GatherlyAPI.swift
+++ Gatherly-SwiftUI/API/GatherlyAPI.swift
@@ -12,9 +12,4 @@ struct GatherlyAPI {
     static func generateID() -> Int {
         Int(Date().timestamp)
     }
-    
-    static func simulateNetworkDelay(seconds: Double = 1.0) async {
-        let ns = UInt64(seconds * 1_000_000_000) // 1 second
-        try? await Task.sleep(nanoseconds: ns)
-    }
 }
