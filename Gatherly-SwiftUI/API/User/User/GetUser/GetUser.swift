43883d009dad6f2b223bcbb14930c692f3ce2597
samialhamad <40968198+samialhamad@users.noreply.github.com>
2025-05-23 15:39:22 -0700

    Create GetUser.swift

---
+++ Gatherly-SwiftUI/API/User/User/GetUser/GetUser.swift
@@ -0,0 +1,20 @@
+//
+//  GetUser.swift
+//  Gatherly-SwiftUI
+//
+//  Created by Sami Alhamad on 5/23/25.
+//
+
+import Combine
+import Foundation
+
+extension GatherlyAPI {
+    static func getUser(withID id: Int = 1) -> AnyPublisher<User?, Never> {
+        let users = UserDefaultsManager.loadUsers()
+        let user = users.first { $0.id == id }
+        
+        return Just(user)
+            .delay(for: .seconds(2), scheduler: DispatchQueue.main)
+            .eraseToAnyPublisher()
+    }
+}
