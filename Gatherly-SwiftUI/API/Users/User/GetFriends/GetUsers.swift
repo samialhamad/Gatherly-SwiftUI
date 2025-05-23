546016e0eb7b3adb57f0d422ba776f8bb4ccf17d
samialhamad <40968198+samialhamad@users.noreply.github.com>
2025-05-23 15:41:42 -0700

    Refactor getUsers to getFriends

---
+++ Gatherly-SwiftUI/API/Users/User/GetFriends/GetUsers.swift
@@ -0,0 +1,27 @@
+//
+//  GetFriends.swift
+//  Gatherly-SwiftUI
+//
+//  Created by Sami Alhamad on 4/14/25.
+//
+
+import Combine
+import Foundation
+
+extension GatherlyAPI {
+    static func getFriends(forUserID id: Int = 1) -> AnyPublisher<[User], Never> {
+        let users = UserDefaultsManager.loadUsers()
+        let currentUser = users.first(where: { $0.id == id })
+        let friends = users.filter { user in
+            guard let userID = user.id else {
+                return false
+            }
+            
+            return currentUser?.friendIDs?.contains(userID) == true
+        }
+        
+        return Just(friends)
+            .delay(for: .seconds(2), scheduler: DispatchQueue.main)
+            .eraseToAnyPublisher()
+    }
+}
