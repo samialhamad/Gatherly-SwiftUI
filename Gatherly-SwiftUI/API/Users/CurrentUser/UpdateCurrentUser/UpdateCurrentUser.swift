0c298f593487121835d2735ce31e81e2b2ae7bf9
samialhamad <40968198+samialhamad@users.noreply.github.com>
2025-05-23 18:08:52 -0700

    Create UpdateCurrentUser.swift

---
+++ Gatherly-SwiftUI/API/Users/CurrentUser/UpdateCurrentUser/UpdateCurrentUser.swift
@@ -0,0 +1,19 @@
+//
+//  UpdateCurrentUser.swift
+//  Gatherly-SwiftUI
+//
+//  Created by Sami Alhamad on 5/23/25.
+//
+
+import Combine
+import Foundation
+
+extension GatherlyAPI {
+    static func updateCurrentUser(_ user: User) -> AnyPublisher<User, Never> {
+        UserDefaultsManager.saveCurrentUser(user)
+        
+        return Just(user)
+            .delay(for: .seconds(2), scheduler: DispatchQueue.main)
+            .eraseToAnyPublisher()
+    }
+}
