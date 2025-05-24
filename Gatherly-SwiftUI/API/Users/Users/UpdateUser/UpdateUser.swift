c5f1c4c1ec147aa91c0de145ec9405742a26b15a
samialhamad <40968198+samialhamad@users.noreply.github.com>
2025-05-23 18:08:51 -0700

    Refactor API updateUser()

--- Gatherly-SwiftUI/API/Users/Users/UpdateUser/UpdateUser.swift
+++ Gatherly-SwiftUI/API/Users/Users/UpdateUser/UpdateUser.swift
@@ -9,16 +9,15 @@ import Combine
 import Foundation
 
 extension GatherlyAPI {
-    static func updateUser(_ user: User) -> AnyPublisher<User, Never> {
+    static func updateUser(_ updatedUser: User) -> AnyPublisher<User, Never> {
         var users = UserDefaultsManager.loadUsers()
         
-        if let id = user.id,
-           let index = users.firstIndex(where: { $0.id == id }) {
-            users[index] = user
+        if let index = users.firstIndex(where: { $0.id == updatedUser.id }) {
+            users[index] = updatedUser
             UserDefaultsManager.saveUsers(users)
         }
         
-        return Just(user)
+        return Just(updatedUser)
             .delay(for: .seconds(2), scheduler: DispatchQueue.main)
             .eraseToAnyPublisher()
     }
