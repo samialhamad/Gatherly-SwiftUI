e45c6cd05b3337a254b7c9bab05fab0ab235a7c8
samialhamad <40968198+samialhamad@users.noreply.github.com>
2025-05-26 16:07:17 -0700

    Create GroupsViewModel.swift

---
+++ Gatherly-SwiftUI/ViewModel/Environments/GroupsViewModel/GroupsViewModel.swift
@@ -0,0 +1,87 @@
+//
+//  GroupsViewModel.swift
+//  Gatherly-SwiftUI
+//
+//  Created by Sami Alhamad on 5/26/25.
+//
+
+import Combine
+import Foundation
+
+final class GroupsViewModel: ObservableObject {
+    @Published var groups: [UserGroup] = []
+    @Published var isLoading = false
+    
+    private var cancellables = Set<AnyCancellable>()
+    private var hasLoaded = false
+    
+    // MARK: - Loaders
+    
+    func loadIfNeeded() {
+        guard !hasLoaded else {
+            return
+        }
+        
+        fetch()
+    }
+    
+    func fetch() {
+        isLoading = true
+        
+        GatherlyAPI.getGroups()
+            .receive(on: RunLoop.main)
+            .sink { [weak self] groups in
+                self?.groups = groups
+                self?.isLoading = false
+                self?.hasLoaded = true
+            }
+            .store(in: &cancellables)
+    }
+    
+    func forceReload() {
+        hasLoaded = false
+        fetch()
+    }
+    
+    // MARK: - CRUD
+    
+    func create(_ group: UserGroup, completion: @escaping (UserGroup) -> Void = { _ in }) {
+        isLoading = true
+        
+        GatherlyAPI.createGroup(group)
+            .receive(on: RunLoop.main)
+            .sink { [weak self] createdUser in
+                self?.groups.append(createdUser)
+                self?.isLoading = false
+                completion(createdUser)
+            }
+            .store(in: &cancellables)
+    }
+    
+    func update(_ group: UserGroup) {
+        isLoading = true
+        
+        GatherlyAPI.updateGroup(group)
+            .receive(on: RunLoop.main)
+            .sink { [weak self] updatedGroup in
+                if let id = updatedGroup.id,
+                   let index = self?.groups.firstIndex(where: { $0.id == id }) {
+                    self?.groups[index] = updatedGroup
+                }
+                self?.isLoading = false
+            }
+            .store(in: &cancellables)
+    }
+    
+    func delete(_ group: UserGroup) {
+        isLoading = true
+        
+        GatherlyAPI.deleteGroup(group)
+            .receive(on: RunLoop.main)
+            .sink { [weak self] newList in
+                self?.groups = newList
+                self?.isLoading = false
+            }
+            .store(in: &cancellables)
+    }
+}
