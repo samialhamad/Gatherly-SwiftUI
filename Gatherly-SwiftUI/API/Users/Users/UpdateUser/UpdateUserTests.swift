7d36e5a81896986c8806e0d7eccdff136c28596a
samialhamad <40968198+samialhamad@users.noreply.github.com>
2025-05-27 19:48:45 -0700

    Update UpdateUserTests.swift

--- Gatherly-SwiftUI/API/Users/Users/UpdateUser/UpdateUserTests.swift
+++ Gatherly-SwiftUI/API/Users/Users/UpdateUser/UpdateUserTests.swift
@@ -5,10 +5,13 @@
 //  Created by Sami Alhamad on 5/8/25.
 //
 
+import Combine
 import XCTest
 @testable import Gatherly_SwiftUI
 
 final class UpdateUserTests: XCTestCase {
+    
+    var cancellables = Set<AnyCancellable>()
     
     override func setUp() {
         super.setUp()
@@ -30,29 +33,54 @@ final class UpdateUserTests: XCTestCase {
         )
     }
     
-    func testUpdateUser_updatesNameCorrectly() async {
+    func testUpdateUser_updatesNameCorrectly() {
+        let expectation = XCTestExpectation(description: "User name is updated")
+        
         var originalUser = makeSampleUser(id: 1)
         UserDefaultsManager.saveUsers([originalUser])
         
         originalUser.firstName = "New"
         originalUser.lastName = "User"
         
-        let updatedUser = await GatherlyAPI.updateUser(originalUser)
+        GatherlyAPI.updateUser(originalUser)
+            .sink { updatedUser in
+                XCTAssertEqual(updatedUser.firstName, "New")
+                XCTAssertEqual(updatedUser.lastName, "User")
+                
+                let storedUsers = UserDefaultsManager.loadUsers()
+                XCTAssertEqual(storedUsers.count, 1)
+                XCTAssertEqual(storedUsers.first?.firstName, "New")
+                XCTAssertEqual(storedUsers.first?.lastName, "User")
+                
+                expectation.fulfill()
+            }
+            .store(in: &cancellables)
         
-        XCTAssertEqual(updatedUser.firstName, "New")
-        XCTAssertEqual(updatedUser.lastName, "User")
+        wait(for: [expectation], timeout: 3)
     }
     
-    func testUpdateUser_updatesAvatarAndBanner() async {
+    func testUpdateUser_updatesAvatarAndBanner() {
+        let expectation = XCTestExpectation(description: "User avatar and banner are updated")
+        
         var originalUser = makeSampleUser(id: 1)
         UserDefaultsManager.saveUsers([originalUser])
         
         originalUser.avatarImageName = "avatar123.png"
         originalUser.bannerImageName = "banner123.png"
         
-        let updatedUser = await GatherlyAPI.updateUser(originalUser)
+        GatherlyAPI.updateUser(originalUser)
+            .sink { updatedUser in
+                XCTAssertEqual(updatedUser.avatarImageName, "avatar123.png")
+                XCTAssertEqual(updatedUser.bannerImageName, "banner123.png")
+                
+                let storedUser = UserDefaultsManager.loadUsers().first
+                XCTAssertEqual(storedUser?.avatarImageName, "avatar123.png")
+                XCTAssertEqual(storedUser?.bannerImageName, "banner123.png")
+                
+                expectation.fulfill()
+            }
+            .store(in: &cancellables)
         
-        XCTAssertEqual(updatedUser.avatarImageName, "avatar123.png")
-        XCTAssertEqual(updatedUser.bannerImageName, "banner123.png")
+        wait(for: [expectation], timeout: 3)
     }
 }
