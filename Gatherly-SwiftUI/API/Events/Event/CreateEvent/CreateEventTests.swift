ed992dad7352553a2c0ae5c45f96b37a4342f89e
samialhamad <40968198+samialhamad@users.noreply.github.com>
2025-05-27 19:46:51 -0700

    Update CreateEventTests.swift

--- Gatherly-SwiftUI/API/Events/Event/CreateEvent/CreateEventTests.swift
+++ Gatherly-SwiftUI/API/Events/Event/CreateEvent/CreateEventTests.swift
@@ -5,23 +5,27 @@
 //  Created by Sami Alhamad on 5/8/25.
 //
 
+import Combine
 import XCTest
 @testable import Gatherly_SwiftUI
 
 final class CreateEventTests: XCTestCase {
-
+    
     let calendar = Calendar.current
-
+    var cancellables = Set<AnyCancellable>()
+    
     override func setUp() {
         super.setUp()
         UserDefaultsManager.removeEvents()
     }
-
-    func testCreateEvent() async {
+    
+    func testCreateEvent() {
+        let expectation = XCTestExpectation(description: "Event creation completes")
+        
         let fixedDate = calendar.date(from: DateComponents(year: 2025, month: 3, day: 5))!
         let startTime = calendar.date(bySettingHour: 10, minute: 0, second: 0, of: fixedDate)!
         let endTime = calendar.date(bySettingHour: 12, minute: 0, second: 0, of: fixedDate)!
-
+        
         var event = Event()
         event.title = "New Event"
         event.description = "Description"
@@ -32,20 +36,29 @@ final class CreateEventTests: XCTestCase {
         event.categories = [.entertainment]
         event.bannerImageName = "test_banner.jpg"
         event.plannerID = 1
-
-        let createdEvent = await GatherlyAPI.createEvent(event)
-
-        XCTAssertEqual(createdEvent.title, "New Event")
-        XCTAssertEqual(createdEvent.description, "Description")
-        XCTAssertEqual(createdEvent.date, calendar.startOfDay(for: fixedDate))
-        XCTAssertEqual(createdEvent.bannerImageName, "test_banner.jpg")
-        XCTAssertGreaterThan(createdEvent.id ?? 0, 0)
-        XCTAssertLessThanOrEqual(createdEvent.id ?? 0, Int(Date().timestamp))
-
-        XCTAssertEqual(createdEvent.startTimestamp, Int(startTime.timestamp))
-        XCTAssertEqual(createdEvent.endTimestamp, Int(endTime.timestamp))
-        XCTAssertEqual(createdEvent.plannerID, 1)
-        XCTAssertEqual(Set(createdEvent.memberIDs ?? []), Set([2, 3]))
-        XCTAssertEqual(createdEvent.categories, [.entertainment])
+        
+        GatherlyAPI.createEvent(event)
+            .sink { createdEvent in
+                XCTAssertEqual(createdEvent.title, "New Event")
+                XCTAssertEqual(createdEvent.description, "Description")
+                XCTAssertEqual(createdEvent.date, self.calendar.startOfDay(for: fixedDate))
+                XCTAssertEqual(createdEvent.bannerImageName, "test_banner.jpg")
+                XCTAssertGreaterThan(createdEvent.id ?? 0, 0)
+                XCTAssertLessThanOrEqual(createdEvent.id ?? 0, Int(Date().timestamp))
+                XCTAssertEqual(createdEvent.startTimestamp, Int(startTime.timestamp))
+                XCTAssertEqual(createdEvent.endTimestamp, Int(endTime.timestamp))
+                XCTAssertEqual(createdEvent.plannerID, 1)
+                XCTAssertEqual(Set(createdEvent.memberIDs ?? []), Set([2, 3]))
+                XCTAssertEqual(createdEvent.categories, [.entertainment])
+                
+                let events = UserDefaultsManager.loadEvents()
+                XCTAssertEqual(events.count, 1)
+                XCTAssertEqual(events.first?.title, "New Event")
+                
+                expectation.fulfill()
+            }
+            .store(in: &cancellables)
+        
+        wait(for: [expectation], timeout: 3)
     }
 }
