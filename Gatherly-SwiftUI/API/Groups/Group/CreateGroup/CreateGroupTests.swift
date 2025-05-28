7343264e555e6007a06d7f8ed7752b7d7724b31f
samialhamad <40968198+samialhamad@users.noreply.github.com>
2025-05-27 19:47:05 -0700

    Update CreateGroupTests.swift

--- Gatherly-SwiftUI/API/Groups/Group/CreateGroup/CreateGroupTests.swift
+++ Gatherly-SwiftUI/API/Groups/Group/CreateGroup/CreateGroupTests.swift
@@ -5,18 +5,22 @@
 //  Created by Sami Alhamad on 5/8/25.
 //
 
+import Combine
 import XCTest
 @testable import Gatherly_SwiftUI
 
-
 final class CreateGroupTests: XCTestCase {
+    
+    var cancellables = Set<AnyCancellable>()
     
     override func setUp() {
         super.setUp()
         UserDefaultsManager.removeGroups()
     }
     
-    func testCreateGroup() async {
+    func testCreateGroup() {
+        let expectation = XCTestExpectation(description: "Group is created and stored in UserDefaults")
+        
         var newGroup = UserGroup(
             bannerImageName: "banner_image.png",
             id: nil,
@@ -27,14 +31,23 @@ final class CreateGroupTests: XCTestCase {
             name: "New Group"
         )
         
-        let createdGroup = await GatherlyAPI.createGroup(newGroup)
+        GatherlyAPI.createGroup(newGroup)
+            .sink { createdGroup in
+                XCTAssertEqual(createdGroup.name, "New Group")
+                XCTAssertNotNil(createdGroup.id)
+                XCTAssertGreaterThan(createdGroup.id ?? 0, 0)
+                XCTAssertEqual(createdGroup.leaderID, 1)
+                XCTAssertEqual(createdGroup.imageName, "group_image.png")
+                XCTAssertEqual(createdGroup.bannerImageName, "banner_image.png")
+                XCTAssertEqual(createdGroup.memberIDs, [2, 3])
+                
+                let storedGroups = UserDefaultsManager.loadGroups()
+                XCTAssertEqual(storedGroups.count, 1)
+                XCTAssertEqual(storedGroups.first?.name, "New Group")
+                expectation.fulfill()
+            }
+            .store(in: &cancellables)
         
-        XCTAssertEqual(createdGroup.name, "New Group")
-        XCTAssertNotNil(createdGroup.id)
-        XCTAssertGreaterThan(createdGroup.id ?? 0, 0)
-        XCTAssertEqual(createdGroup.leaderID, 1)
-        XCTAssertEqual(createdGroup.imageName, "group_image.png")
-        XCTAssertEqual(createdGroup.bannerImageName, "banner_image.png")
-        XCTAssertEqual(createdGroup.memberIDs, [2, 3])
+        wait(for: [expectation], timeout: 3)
     }
 }
