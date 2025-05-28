737069b1e8d1abf41a0fce37d3e2ffd315477227
samialhamad <40968198+samialhamad@users.noreply.github.com>
2025-05-27 19:47:11 -0700

    Update UpdateGroupTests.swift

--- Gatherly-SwiftUI/API/Groups/Group/UpdateGroup/UpdateGroupTests.swift
+++ Gatherly-SwiftUI/API/Groups/Group/UpdateGroup/UpdateGroupTests.swift
@@ -5,26 +5,33 @@
 //  Created by Sami Alhamad on 5/8/25.
 //
 
+import Combine
 import XCTest
 @testable import Gatherly_SwiftUI
 
 final class UpdateGroupTests: XCTestCase {
+    
+    var cancellables = Set<AnyCancellable>()
     
     override func setUp() {
         super.setUp()
         UserDefaultsManager.removeGroups()
     }
     
-    func testUpdateGroup() async {
-        var original = UserGroup(
-            id: nil,
+    func testUpdateGroup() {
+        let expectation = XCTestExpectation(description: "Group is updated and saved")
+        
+        let original = UserGroup(
+            bannerImageName: "banner.png",
+            id: 123,
+            imageName: "original.png",
             leaderID: 4,
             memberIDs: [1, 2],
             messages: [],
             name: "Original Name"
         )
         
-        original = await GatherlyAPI.createGroup(original)
+        UserDefaultsManager.saveGroups([original])
         
         var updated = original
         updated.name = "Updated Name"
@@ -32,14 +39,23 @@ final class UpdateGroupTests: XCTestCase {
         updated.imageName = "new_image.png"
         updated.bannerImageName = "new_banner.png"
         
-        let result = await GatherlyAPI.updateGroup(updated)
+        GatherlyAPI.updateGroup(updated)
+            .sink { result in
+                XCTAssertEqual(result.id, original.id)
+                XCTAssertEqual(result.leaderID, original.leaderID)
+                XCTAssertEqual(result.name, "Updated Name")
+                XCTAssertEqual(result.imageName, "new_image.png")
+                XCTAssertEqual(result.bannerImageName, "new_banner.png")
+                XCTAssertEqual(result.memberIDs, [1, 2, 3])
+                XCTAssertEqual(result.messages ?? [], original.messages ?? [])
+                
+                let storedGroups = UserDefaultsManager.loadGroups()
+                XCTAssertEqual(storedGroups.count, 1)
+                XCTAssertEqual(storedGroups.first?.name, "Updated Name")
+                expectation.fulfill()
+            }
+            .store(in: &cancellables)
         
-        XCTAssertEqual(result.id, original.id)
-        XCTAssertEqual(result.leaderID, original.leaderID)
-        XCTAssertEqual(result.name, "Updated Name")
-        XCTAssertEqual(result.imageName, "new_image.png")
-        XCTAssertEqual(result.bannerImageName, "new_banner.png")
-        XCTAssertEqual(result.memberIDs, [1, 2, 3])
-        XCTAssertEqual(result.messages ?? [], original.messages ?? [])
+        wait(for: [expectation], timeout: 2)
     }
 }
