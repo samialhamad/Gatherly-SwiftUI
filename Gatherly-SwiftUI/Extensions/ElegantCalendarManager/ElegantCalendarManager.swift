d07a15f1f67f4200ab945bd8ea682c969e6412c1
samialhamad <40968198+samialhamad@users.noreply.github.com>
2025-05-30 16:00:24 -0700

    Remove ElegantCalendarManager selectedDate

--- Gatherly-SwiftUI/Extensions/ElegantCalendarManager/ElegantCalendarManager.swift
+++ Gatherly-SwiftUI/Extensions/ElegantCalendarManager/ElegantCalendarManager.swift
@@ -10,19 +10,16 @@ import Foundation
 import SwiftUI
 
 extension ElegantCalendarManager {
-    static func withEvents(selectedDate: Binding<Date>, events: Binding<[Event]>, navigationState: NavigationState) -> ElegantCalendarManager {
+    static func withEvents(events: Binding<[Event]>, navigationState: NavigationState) -> ElegantCalendarManager {
         let now = Date()
         let config = CalendarConfiguration(
             startDate: now.minus(calendarComponent: .day, value: 365) ?? now,
             endDate: now.plus(calendarComponent: .day, value: 365) ?? now
         )
         
-        let manager = ElegantCalendarManager(configuration: config,
-                                             initialMonth: selectedDate.wrappedValue)
+        let manager = ElegantCalendarManager(configuration: config, initialMonth: now)
         
-        manager.delegate = EventHighlightDelegate(selectedDate: selectedDate,
-                                                  events: events,
-                                                  navigationState: navigationState
+        manager.delegate = EventHighlightDelegate(events: events, navigationState: navigationState
         )
         
         return manager
