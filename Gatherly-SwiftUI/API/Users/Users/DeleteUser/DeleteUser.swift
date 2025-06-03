034316690f4c86b52532764c51d3e55c3568cd68
samialhamad <40968198+samialhamad@users.noreply.github.com>
2025-06-03 12:49:37 -0700

    Rename userToDelete

--- Gatherly-SwiftUI/API/Users/Users/DeleteUser/DeleteUser.swift
+++ Gatherly-SwiftUI/API/Users/Users/DeleteUser/DeleteUser.swift
@@ -9,11 +9,11 @@ import Combine
 import Foundation
 
 extension GatherlyAPI {
-    static func deleteUser(_ userToDelete: User) -> AnyPublisher<Bool, Never> {
+    static func deleteUser(_ user: User) -> AnyPublisher<Bool, Never> {
         var users = UserDefaultsManager.loadUsers()
         let originalCount = users.count
         
-        users.removeAll { $0.id == userToDelete.id }
+        users.removeAll { $0.id == user.id }
         UserDefaultsManager.saveUsers(users)
         
         return Just(users.count < originalCount)
