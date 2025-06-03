a6ea8dbf5344d38e6992011a7039406df043f798
samialhamad <40968198+samialhamad@users.noreply.github.com>
2025-06-03 12:49:33 -0700

    Rename groupToDelete

--- Gatherly-SwiftUI/API/Groups/Group/DeleteGroup/DeleteGroup.swift
+++ Gatherly-SwiftUI/API/Groups/Group/DeleteGroup/DeleteGroup.swift
@@ -9,11 +9,11 @@ import Combine
 import Foundation
 
 extension GatherlyAPI {
-    static func deleteGroup(_ groupToDelete: UserGroup) -> AnyPublisher<Bool, Never> {
+    static func deleteGroup(_ group: UserGroup) -> AnyPublisher<Bool, Never> {
         var groups = UserDefaultsManager.loadGroups()
         let originalCount = groups.count
         
-        groups.removeAll { $0.id == groupToDelete.id }
+        groups.removeAll { $0.id == group.id }
         UserDefaultsManager.saveGroups(groups)
         
         return Just(groups.count < originalCount)
